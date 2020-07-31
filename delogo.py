6feb33df246a9902945854407bc8b67498649a34
mintaka33 <mintaka33@outlook.com>
2020-07-31 22:14:20 +0800

    refine logo selection

--- delogo.py
+++ delogo.py
@@ -5,17 +5,6 @@ imgfile = '1.jpg'
 refPt = []
 cropping = False
 
-def click_and_crop(event, x, y, flags, param):
-    global refPt, cropping
-    if event == cv2.EVENT_LBUTTONDOWN:
-        refPt = [(x, y)]
-        cropping = True
-    elif event == cv2.EVENT_LBUTTONUP:
-        refPt.append((x, y))
-        cropping = False
-        cv2.rectangle(image2, refPt[0], refPt[1], (0, 255, 0), 2)
-        cv2.imshow("image", image2)
-
 image = cv2.imread(imgfile)
 (h, w, _) = image.shape
 mask = np.zeros((h, w), np.uint8) * 255
@@ -26,17 +15,12 @@ image2 = cv2.resize(image, (w2, h2), cv2.INTER_AREA)
 clone = image2.copy()
 
 cv2.namedWindow("image")
-cv2.setMouseCallback("image", click_and_crop)
-
-while True:
-    cv2.imshow("image", image2)
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("r"): # press 'r' to re-select logo
-        image2 = clone.copy()
-    elif key == 13: # press enter to continue
-        break
-
+cv2.imshow("image", image2)
+bb = cv2.selectROI("image", image2, fromCenter=False, showCrosshair=True)
+refPt.append((bb[0], bb[1]))
+refPt.append((bb[0]+bb[2], bb[1]+bb[3]))
 cv2.destroyAllWindows()
+
 srcvideo = imgfile # './test/IMG_4027.mp4'
 cap = cv2.VideoCapture(srcvideo)
 if not cap.isOpened():
